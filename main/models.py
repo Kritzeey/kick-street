9e7f8d0875ed7d51516c9b609d3eda83162d295d
Kritzeey <190005533+Kritzeey@users.noreply.github.com>
2025-09-17 00:47:24 +0700

    feat: add main.html template

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    description = models.TextField()
+    thumbnail = models.URLField()
+    price = models.IntegerField()
+    is_featured = models.BooleanField()
+
+    def __str__(self):
+        return self.name
