0019affec086a629beb47c2b746bc306338cc493
Kritzeey <190005533+Kritzeey@users.noreply.github.com>
2025-09-17 10:51:53 +0700

    fix: fix form issue

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "brand", "category", "description", "thumbnail", "price", "is_featured"]
+        fields = ["name", "brand", "category", "description", "thumbnail", "price", "stock", "is_featured"]
