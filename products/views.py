bc6f37cf1eaa7f1d3002353ed21b4135b37195a0
Kritzeey <190005533+Kritzeey@users.noreply.github.com>
2025-10-06 14:10:43 +0700

    feat: add product creation page skeleton

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from products.forms import ProductForm
+
+def show_create_product(request):
+    form = ProductForm(request.POST)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+
+        return redirect("main:show_main")
+    
+    context = { "form": form }
+
+    return render(request, "create_product.html", context)
