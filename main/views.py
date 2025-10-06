84a9300e4dfec757bf11fd441553a1d483d580b1
Kritzeey <190005533+Kritzeey@users.noreply.github.com>
2025-10-06 15:42:58 +0700

    feat: add featured products section

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
-# Create your views here.
 def show_main(request):
+    featured_products = Product.objects.filter(is_featured=True)
+
     context = {
         "app_name": "Kick Street",
         "name": "Valerian Hizkia Emmanuel",
         "class": "PBP E",
+        "products": featured_products,
     }
 
     return render(request, "main.html", context)
