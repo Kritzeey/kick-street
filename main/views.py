d45f92b221a20fc025823928bd4d55f72adb49e6
Kritzeey <190005533+Kritzeey@users.noreply.github.com>
2025-10-06 13:32:43 +0700

    feat: add footer

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        "npm": 2406495382,
+        "app_name": "Kick Street",
         "name": "Valerian Hizkia Emmanuel",
-        "class": "PBP E"
+        "class": "PBP E",
     }
 
     return render(request, "main.html", context)
