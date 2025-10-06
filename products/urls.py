4f1c7e9f958ef6f3f04f4072a0e06635b8c8cde9
Kritzeey <190005533+Kritzeey@users.noreply.github.com>
2025-10-06 14:29:10 +0700

    feat: add different serializers for products

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from products.views import show_create_product
-
+from products.views import show_create_product, show_product_json_by_id, show_product_xml_by_id, show_products_json, show_products_xml
 
 app_name = "products"
 
 urlpatterns = [
-    path("create/", show_create_product, name="show_create_product")
+    path("create/", show_create_product, name="show_create_product"),
+    path("json/", show_products_json, name="show_json"),
+    path("xml/", show_products_xml, name="xml"),
+    path("json/<str:news_id>/", show_product_json_by_id, name="show_product_json_by_id"),
+    path("xml/<str:news_id>/", show_product_xml_by_id, name="show_product_xml_by_id"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
-
 from products.forms import ProductForm
+from products.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_create_product(request):
     form = ProductForm(request.POST)
@@ -13,3 +15,31 @@ def show_create_product(request):
     context = { "form": form }
 
     return render(request, "create_product.html", context)
+
+def show_products_json(request):
+    products = Product.objects.all()
+
+    data = serializers.serialize("json", products)
+
+    return HttpResponse(data, content_type="application/json")
+
+def show_products_xml(request):
+    products = Product.objects.all()
+
+    data = serializers.serialize("xml", products)
+
+    return HttpResponse(data, content_type="application/xml")
+
+def show_product_json_by_id(request, product_id):
+    product = Product.objects.get(pk=product_id)
+
+    data = serializers.serialize("json", [product])
+
+    return HttpResponse(data, content_type="application/json")
+
+def show_product_xml_by_id(request, product_id):
+    product = Product.objects.get(pk=product_id)
+
+    data = serializers.serialize("xml", [product])
+
+    return HttpResponse(data, content_type="application/xml")
